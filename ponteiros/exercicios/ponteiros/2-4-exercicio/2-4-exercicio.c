15aee2ebf4e1ab49fdf428598d0bb3cff1d8bf49
JoaoVictorSicco <victordesicco@gmail.com>
2023-06-19 17:26:09 -0400

    Adição do exercicio 2.4 ponteiros

---
+++ ponteiros/exercicios/ponteiros/2-4-exercicio/2-4-exercicio.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/*
+2.4. Escreva uma função maior_valor que recebe um ponteiro para um vetor de inteiros 
+e seu tamanho como argumentos, e retorna o maior valor presente no vetor.
+
+*/
+
+
+
+int main()
+{
+
+}
